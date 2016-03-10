54dd27ffb345f653872b20c77f4645686554d7be
Alex Lock <alock@groupon.com>
2016-03-09 18:04:49 -0600

    database config fixes

--- docker-build.sh
+++ docker-build.sh
@@ -1,10 +1,6 @@
 docker-compose build
-docker-compose start db
-sleep 3
-docker-compose run db mysqladmin -u root -h db create codeburner_production
 docker-compose run web rake db:setup RAILS_ENV=production
 
 if [ $? == 0 ]; then
-  CODEBURNER_URL="http://$(docker-machine ip):3000/"
   echo "Docker containers initialized, run 'docker-compose up' to start Codeburner."
 fi
