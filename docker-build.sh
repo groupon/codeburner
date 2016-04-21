11aa113943e726d05a487df2b8a39a9d05950b67
Alex Lock <alock@groupon.com>
2016-04-21 11:43:03 -0500

    docker fix

--- docker-build.sh
+++ docker-build.sh
@@ -1,5 +1,5 @@
 docker-compose build
-docker-compose run web bash -c "while ! mysqladmin ping -h db --silent; do echo waiting for mysql; sleep 3; done; rake db:create; rake db:schema:load"
+docker-compose run web bash -c "while ! mysqladmin ping -h db --silent; do echo waiting for mysql; sleep 3; done; mysqladmin -u root -h db create codeburner_production"
 docker-compose run web rake db:setup
 
 if [ $? == 0 ]; then
