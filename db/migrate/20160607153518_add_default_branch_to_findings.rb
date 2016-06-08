11feac90e181d1fd616ea82c460b1df63ca52b4c
Alex Lock <alock@groupon.com>
2016-06-08 09:10:26 -0500

    migration fix

--- db/migrate/20160607153518_add_default_branch_to_findings.rb
+++ db/migrate/20160607153518_add_default_branch_to_findings.rb
@@ -1,7 +1,7 @@
 class AddDefaultBranchToFindings < ActiveRecord::Migration
   def change
     Finding.all.each do |finding|
-      finding.update(:branch => finding.repo.branches.first)
+      finding.update(:branch => finding.repo.branches.first) unless finding.branch
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160603165144) do
+ActiveRecord::Schema.define(version: 20160607153518) do
 
   create_table "branches", force: :cascade do |t|
     t.integer  "repo_id",    limit: 4
